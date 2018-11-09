9bd0e4549816640428cd30671a7b811e582d04eb
fukangwei <15262751239@163.com>
2018-11-09 13:20:40 +0900

    fix AT24C64 Driver

--- STM32/AT24C64/HARDWARE/IIC/myiic.c
+++ STM32/AT24C64/HARDWARE/IIC/myiic.c
@@ -1,76 +1,59 @@
 #include "myiic.h"
 #include "delay.h"
 
-//初始化IIC
-void IIC_Init ( void )
-{
+void IIC_Init ( void ) { /* 初始化IIC */
     GPIO_InitTypeDef GPIO_InitStructure;
-    //RCC->APB2ENR|=1<<4;//先使能外设IO PORTC时钟
-    
     RCC_APB2PeriphClockCmd ( RCC_APB2Periph_GPIOB, ENABLE );
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP ;   //推挽输出
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP ; /* 推挽输出 */
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init ( GPIOB, &GPIO_InitStructure );
-    
     IIC_SCL = 1;
     IIC_SDA = 1;
 }
 
-//产生IIC起始信号
-void IIC_Start ( void )
-{
-    SDA_OUT();     //sda线输出
+void IIC_Start ( void ) { /* 产生IIC起始信号 */
+    SDA_OUT(); /* sda线输出 */
     IIC_SDA = 1;
     IIC_SCL = 1;
     delay_us ( 4 );
-    IIC_SDA = 0; //START:when CLK is high,DATA change form high to low
+    IIC_SDA = 0; /* START: when CLK is high, DATA change form high to low */
     delay_us ( 4 );
-    IIC_SCL = 0; //钳住I2C总线，准备发送或接收数据
+    IIC_SCL = 0; /* 钳住I2C总线，准备发送或接收数据 */
 }
 
-//产生IIC停止信号
-void IIC_Stop ( void )
-{
-    SDA_OUT();//sda线输出
+void IIC_Stop ( void ) { /* 产生IIC停止信号 */
+    SDA_OUT(); /* sda线输出 */
     IIC_SCL = 0;
-    IIC_SDA = 0; //STOP:when CLK is high DATA change form low to high
+    IIC_SDA = 0; /* STOP: when CLK is high DATA change form low to high */
     delay_us ( 4 );
     IIC_SCL = 1;
-    IIC_SDA = 1; //发送I2C总线结束信号
+    IIC_SDA = 1; /* 发送I2C总线结束信号 */
     delay_us ( 4 );
 }
 
-//等待应答信号到来
-//返回值：1，接收应答失败
-//        0，接收应答成功
-u8 IIC_Wait_Ack ( void )
-{
+u8 IIC_Wait_Ack ( void ) { /* 等待应答信号到来，返回值1表示接收应答失败，0表示成功 */
     u8 ucErrTime = 0;
-    SDA_IN();      //SDA设置为输入
+    SDA_IN(); /* SDA设置为输入 */
     IIC_SDA = 1;
     delay_us ( 1 );
     IIC_SCL = 1;
     delay_us ( 1 );
 
-    while ( READ_SDA )
-    {
+    while ( READ_SDA ) {
         ucErrTime++;
 
-        if ( ucErrTime > 250 )
-        {
+        if ( ucErrTime > 250 ) {
             IIC_Stop();
             return 1;
         }
     }
 
-    IIC_SCL = 0; //时钟输出0
+    IIC_SCL = 0; /* 时钟输出0 */
     return 0;
 }
 
-//产生ACK应答
-void IIC_Ack ( void )
-{
+void IIC_Ack ( void ) { /* 产生ACK应答 */
     IIC_SCL = 0;
     SDA_OUT();
     IIC_SDA = 0;
@@ -79,9 +62,8 @@ void IIC_Ack ( void )
     delay_us ( 2 );
     IIC_SCL = 0;
 }
-//不产生ACK应答
-void IIC_NAck ( void )
-{
+
+void IIC_NAck ( void ) { /* 不产生ACK应答 */
     IIC_SCL = 0;
     SDA_OUT();
     IIC_SDA = 1;
@@ -91,21 +73,15 @@ void IIC_NAck ( void )
     IIC_SCL = 0;
 }
 
-//IIC发送一个字节
-//返回从机有无应答
-//1，有应答
-//0，无应答
-void IIC_Send_Byte ( u8 txd )
-{
+void IIC_Send_Byte ( u8 txd ) { /* IIC发送一个字节，返回从机有无应答，1表示有应答，0表示无应答 */
     u8 t;
     SDA_OUT();
-    IIC_SCL = 0; //拉低时钟开始数据传输
+    IIC_SCL = 0; /* 拉低时钟开始数据传输 */
 
-    for ( t = 0; t < 8; t++ )
-    {
+    for ( t = 0; t < 8; t++ ) {
         IIC_SDA = ( txd & 0x80 ) >> 7;
         txd <<= 1;
-        delay_us ( 2 ); //对TEA5767这三个延时都是必须的
+        delay_us ( 2 );
         IIC_SCL = 1;
         delay_us ( 2 );
         IIC_SCL = 0;
@@ -113,35 +89,27 @@ void IIC_Send_Byte ( u8 txd )
     }
 }
 
-//读1个字节，ack=1时，发送ACK，ack=0，发送nACK
-u8 IIC_Read_Byte ( unsigned char ack )
-{
+u8 IIC_Read_Byte ( unsigned char ack ) { /* 读1个字节，当ack为1时，发送ACK；ack为0时，发送nACK */
     unsigned char i, receive = 0;
-    SDA_IN();//SDA设置为输入
+    SDA_IN(); /* SDA设置为输入 */
 
-    for ( i = 0; i < 8; i++ )
-    {
+    for ( i = 0; i < 8; i++ ) {
         IIC_SCL = 0;
         delay_us ( 2 );
         IIC_SCL = 1;
         receive <<= 1;
 
-        if ( READ_SDA )
-        {
+        if ( READ_SDA ) {
             receive++;
         }
 
         delay_us ( 1 );
     }
 
-    if ( !ack )
-    {
-        IIC_NAck();    //发送nACK
-    }
-
-    else
-    {
-        IIC_Ack();    //发送ACK
+    if ( !ack ) {
+        IIC_NAck(); /* 发送nACK */
+    } else {
+        IIC_Ack(); /* 发送ACK */
     }
 
     return receive;
